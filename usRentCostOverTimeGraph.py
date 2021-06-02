adfa178ed8e4a632d50e1a5ea581584c78389d82
Shuen Yasui <shuenyasui@gmail.com>
2021-06-01 21:06:16 -0700

    Sanitized string in names col, set xaxis to display names, stylize xaxis

--- usRentCostOverTimeGraph.py
+++ usRentCostOverTimeGraph.py
@@ -1,15 +1,26 @@
+import math
 import pandas as pd
-from bokeh.plotting import figure, show
+from bokeh.io import show
+from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
+# Prepare data format using pandas
+# Open csv, and filter data
 df=pd.read_csv('censusData\\2010.csv', header=1, usecols=lambda x:"Percent!!GROSS RENT!!" in x and "$" in x and "Occupied" not in x)
-df2=df.transpose()
-df2=df2.reset_index()
-df2.columns=["Name","Percent"]
-print(df2)
-for row in df2.iterrows():
-    print(row)
-sc=ColumnDataSource(df2)
-p=figure()
-p.vbar(x='index', top='Percent', source=sc, width=0.70)
+dfTrans=df.transpose()
+dfTrans=dfTrans.reset_index()
+# define column names
+dfTrans.columns=["Name","Percent"]
+# sanitize string in name
+dfTrans['Name']=dfTrans['Name'].str.replace("Percent!!GROSS RENT!!","")
+
+print(dfTrans)
+
+# Create plot using bokeh
+sc=ColumnDataSource(dfTrans)
+# Define figure with x range as Name
+p=figure(x_range=dfTrans['Name'])
+p.vbar(x='Name', top='Percent', source=sc, width=0.70)
+# Stylize x axis
+p.xaxis.major_label_orientation = math.pi/4
 show(p)
