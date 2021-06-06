bf1e8caafd77b43390cdf548dc4d8b78f62c8097
Shuen Yasui <shuenyasui@gmail.com>
2021-06-05 17:34:37 -0700

    Append each .csv data to allDF and concat at end. Added graph annotations

--- usRentCostOverTimeGraph.py
+++ usRentCostOverTimeGraph.py
@@ -5,39 +5,36 @@ from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 
 # Prepare data format using pandas
-# Open csv, and filter data
-years=[]
 cYear=2010
+allDF=[]
+# Cycle through all .csv data between 2010-2019
 while cYear<2020:
-    if cYear==2018:
-        temp_df=pd.read_csv('censusData\\'+str(cYear)+".csv", header=1, usecols=lambda x:"Percent Estimate!!GROSS RENT!!" in x and "$" in x)
+    # Open csv, and extract data
+    if cYear==2018: # specific filters due to csv format
+        c_df=pd.read_csv('censusData\\'+str(cYear)+".csv", header=1, usecols=lambda x:"Percent Estimate!!GROSS RENT!!" in x and "$" in x)
     else:
-        temp_df=pd.read_csv('censusData\\'+str(cYear)+".csv", header=1, usecols=lambda x:"Percent!!GROSS RENT!!" in x and "$" in x)
-    temp_df=temp_df.transpose()
-    temp_df=temp_df.reset_index()
-    print('censusData\\'+str(cYear)+".csv")
-    temp_df.columns=["Name","Percent"]
+        c_df=pd.read_csv('censusData\\'+str(cYear)+".csv", header=1, usecols=lambda x:"Percent!!GROSS RENT!!" in x and "$" in x)
+    c_df=c_df.transpose()
+    c_df=c_df.reset_index()
+    c_df.columns=["Name","Percent"]
     # sanitize string in name
-    temp_df['Name']=temp_df['Name'].str.replace("Percent!!GROSS RENT!!","")
-    temp_df['Name']=temp_df['Name'].str.replace("Occupied units paying rent!!","")
-    temp_df['Name']=temp_df['Name'].str.replace("Percent Estimate!!GROSS RENT!!","")
-    print(temp_df)
+    c_df['Name']=c_df['Name'].str.replace("Percent!!GROSS RENT!!","")
+    c_df['Name']=c_df['Name'].str.replace("Occupied units paying rent!!","")
+    c_df['Name']=c_df['Name'].str.replace("Percent Estimate!!GROSS RENT!!","")
+    c_df['Name']=str(cYear)+" "+c_df['Name'] # Add year prefix to name
+    # append df to allDF array
+    allDF.append(c_df)
     cYear+=1
-df=pd.read_csv('censusData\\2010.csv', header=1, usecols=lambda x:"Percent!!GROSS RENT!!" in x and "$" in x and "Occupied" not in x)
-dfTrans=df.transpose()
-dfTrans=dfTrans.reset_index()
-# define column names
-dfTrans.columns=["Name","Percent"]
-# sanitize string in name
-dfTrans['Name']=dfTrans['Name'].str.replace("Percent!!GROSS RENT!!","")
-
-print(dfTrans)
-
+contDF=pd.concat(allDF)
 # Create plot using bokeh
-sc=ColumnDataSource(dfTrans)
+sc=ColumnDataSource(contDF)
 # Define figure with x range as Name
-p=figure(x_range=dfTrans['Name'])
-p.vbar(x='Name', top='Percent', source=sc, width=0.70)
-# Stylize x axis
+p=figure(x_range=contDF['Name'], plot_width=1500)
+# Define graph annotations
+p.title.text="Rent cost catagory by year"
+p.xaxis.axis_label="Year and rent cost bracket"
 p.xaxis.major_label_orientation = math.pi/4
-#show(p)
+p.yaxis.axis_label="Percent"
+# Create bar graph
+p.vbar(x='Name', top='Percent', source=sc, width=0.7)
+show(p)
